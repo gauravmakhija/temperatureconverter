5e405a9442ed8e6bfac66a7c322d323551375392
Gaurav Makhija <gauravmakhija@yahoo.com>
2014-01-13 23:43:09 -0800

    Initial Commit

---
+++ temperatureconvertergitTests/temperatureconvertergitTests.m
@@ -0,0 +1,34 @@
+//
+//  temperatureconvertergitTests.m
+//  temperatureconvertergitTests
+//
+//  Created by Gaurav Makhija on 1/13/14.
+//  Copyright (c) 2014 Maps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface temperatureconvertergitTests : XCTestCase
+
+@end
+
+@implementation temperatureconvertergitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
